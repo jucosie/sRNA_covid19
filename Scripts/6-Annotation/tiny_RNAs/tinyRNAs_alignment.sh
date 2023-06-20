ddee526694b1cca7a5ef38e51aa35c0514ec74b6
Julia Corell <64853438+jucosie@users.noreply.github.com>
2023-06-20 09:55:46 +0200

    Update tinyRNAs_alignment.sh

--- Scripts/6-Annotation/tiny_RNAs/tinyRNAs_alignment.sh
+++ Scripts/6-Annotation/tiny_RNAs/tinyRNAs_alignment.sh
@@ -9,5 +9,5 @@ results_dir="/path/to/annotation_results/tiny_RNAs"
 cd $results_dir
 
 scram_linux profile -r miRBase_mature_human_final.fa -1 sequences_tocheck_tiny.fa -t fa --minLen 13 -o tinyRNA -l 13,14
-scram_plot.py profile -a tinyRNA -l 13,14 -png
+scram_plot.py profile -a tinyRNA -l 13,14 -png #If you don't need to visualize every plot comment this line, as it will run interactively a plot for every alignment
 
